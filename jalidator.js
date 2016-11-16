fe90ae2db64d76906b750c729866d5c3c752bf9c
Yami Odymel <yamiodymel@gmail.com>
2016-11-17 01:22:08 +0800

    Update jalidator.js

--- jalidator.js
+++ jalidator.js
@@ -147,7 +147,11 @@ export function validate (rule, data, init)
                     if(!ipv6)
                         res[n].type = true
                     break
-                case 'url'  : break
+                case 'url'  :
+                    regex = new RegExp(`/^((https:|http:|ftp:|rtsp:|mms:)?//)?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z_!~*'()-]+\.)*([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\.[a-z]{2,6}|localhost)(:[0-9]{1,4})?((/?)|(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$/`)
+                    if(!regex.test(d))
+                        res[n].type = true
+                    break
             }
         }
     }
